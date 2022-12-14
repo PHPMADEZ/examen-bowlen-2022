6f58298289abbda8e6f393ee0de5349fe8aec767
BerkanHRGL <329918@student.mboutrecht.nl>
2022-12-14 12:31:41 +0100

    Klant email/details bewerken

--- app/controllers/Klanten.php
+++ app/controllers/Klanten.php
@@ -34,6 +34,7 @@ class Klanten extends Controller
                         <td>" . $value->Mobiel . "</td>
                         <td>" . $value->Email . "</td>
                         <td>" . $value->IsVolwassen . "</td>
+                        <td><a href='" . URLROOT . "/klanten/update/$value->Id'><i class='bi bi-pencil-square'></i></a></td>
                         </tr>";
         }
 
@@ -44,5 +45,124 @@ class Klanten extends Controller
         ];
         $this->view('klanten/index', $data);
     }
+
+    public function update($Id = null)
+    {
+        /**
+         * Check of we van het update formulier komen
+         */
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+            $data = [
+                'title' => '<h3>Wijzig reservering</h3>',
+                'Id' => trim($_POST['Id']),
+                'Voornaam' => trim($_POST['Voornaam']),
+                'Tussenvoegsel' => trim($_POST['Tussenvoegsel']),
+                'Achternaam' => trim($_POST['Achternaam']),
+                'Mobiel' => trim($_POST['Mobiel']),
+                'Email' => trim($_POST['Email']),
+                'IsVolwassen' => trim($_POST['IsVolwassen']),
+                'VoornaamError' => '',
+                'AchternaamError' => '',
+                'MobielError' => '',
+                'EmailError' => '',
+                'optieError' => '',
+                'IsVolwassenError' => ''
+            ];
+
+            /**
+             * Valideer de ingevulde formulier waarden met de method
+             */
+            $data = $this->validateCreateForm($data);
+
+            /**
+             * Check of er geen validatie error is
+             */
+            if (
+                empty($data['VoornaamError']) && 
+                empty($data['AchternaamError']) && 
+                empty($data['MobielError']) && 
+                empty($data['EmailError']) &&
+                empty($data['IsVolwassenError'])
+            ) {
+        
+                /**
+                 * Als er een update heeft plaatsgevonden
+                 */
+                if ($this->klantModel->updateKlant($data)) {
+                    /**
+                     * Dan een melding dat de gegevens zijn gewijzigd
+                     */
+                    echo "<div class='alert alert-success' role='alert'>
+                            Uw gegevens zijn gewijzigd.
+                        </div>";
+                    header("Refresh:3; url=" . URLROOT . "/klanten/index");
+                } else {
+                    /**
+                     * Anders de melding dat er een interne servererror heeft plaatsgevonden
+                     */
+                    echo "<div class='alert alert-danger' role='alert'>
+                            Er heeft een interne servererror plaatsgevonden<br>probeer het later nog eens...
+                        </div>";
+                    header("Refresh:3; url=" . URLROOT . "/klanten/index");
+                }
+            }
+            /**
+             * Stuur het $data array met de validatie error meldingen naar de pagina update
+             */
+            $this->view("klanten/update", $data);
+        } else {
+            /**
+             * Wanneer we van de reserveringen/index.php pagina afkomen dan sturen we het opgehaalde 
+             * record naar de reserveringen/update.php pagina d.m.v. het $data array.
+             */
+            $row = $this->klantModel->getSingleKlant($Id);
+            //var_dump($row);exit();
+            $data = [
+                'title' => '<h3>Update reservering</h3>',
+                'Id' => $row->Id,
+                'Voornaam' => $row->Voornaam,
+                'Tussenvoegsel' => $row->Tussenvoegsel,
+                'Achternaam' => $row->Achternaam,
+                'Mobiel' => $row->Mobiel,
+                'Email' => $row->Email,
+                'IsVolwassen' => $row->IsVolwassen,
+                'VoornaamError' => '',
+                'AchternaamError' => '',
+                'MobielError' => '',
+                'EmailError' => '',
+                'IsVolwassenError' => ''
+            ];
+            
+            $this->view("klanten/update", $data);
+            
+        }    
+        
+        
+    }
+    private function validateCreateForm($data) {
+        if (empty($data['Voornaam'])) {
+        $data['VoornaamError'] = 'U heeft nog geen voornaam ingevuld';
+        }
+
+        if (empty($data['Achternaam'])) {
+        $data['AchternaamError'] = 'U heeft nog geen achternaam ingevuld';
+        }
+
+        if (empty($data['Mobiel'])) {
+            $data['MobielError'] = 'U heeft nog geen telefoonnummer ingevuld';
+            }
+
+        if (empty($data['Email'])) {
+            $data['EmailError'] = 'U heeft nog geen e-mail ingevuld';
+            }
+        if (empty($data['IsVolwassen'])) {
+            $data['IsVolwassenError'] = 'U heeft nog geen optie ingevuld';
+            }
+        return $data;
+    }
+
 
 }
--- app/models/Klant.php
+++ app/models/Klant.php
@@ -25,6 +25,59 @@
       $result = $this->db->resultSet();
       return $result;
     }
+     public function getSingleKlant($Id) {
+    try {
+      $this->db->query("SELECT persoon.Id
+                              , persoon.Voornaam
+                              , persoon.Tussenvoegsel
+                              , persoon.Achternaam
+                              , persoon.IsVolwassen 
+                              , contact.Mobiel
+                              , contact.Email
+                        FROM persoon 
+                        INNER JOIN contact 
+                        ON persoon.Id = contact.PersoonId 
+                        WHERE persoon.Id = :Id");
+      $this->db->bind(':Id', $Id, PDO::PARAM_INT);
+      return $this->db->single();
+    } catch(PDOException $e) {
+      logger(__FILE__, __METHOD__, __LINE__, $e->getMessage());
+      return 0;
+  }
+    }
+
+    public function updateKlant($post) {
+        // var_dump($post);exit();
+      // $this->db->query("UPDATE reservering, klant
+      //                   SET voornaam = :voornaam,
+      //                       achternaam = :achternaam,
+      //                       tijd = :tijd,
+      //                       datum = :datum,
+      //                       optie = :optie
+      //                   WHERE klant.id = :id");
+
+      $this->db->query("UPDATE persoon
+                        INNER JOIN contact
+                        ON persoon.Id = contact.PersoonId
+                        SET Voornaam = :Voornaam,
+                            Tussenvoegsel = :Tussenvoegsel,
+                            Achternaam = :Achternaam,
+                            IsVolwassen = :IsVolwassen,
+                            Mobiel = :Mobiel,
+                            Email = :Email
+                        WHERE persoon.Id = :Id");
+
+      $this->db->bind(':Id', $post["Id"], PDO::PARAM_INT);
+      $this->db->bind(':Voornaam', $post["Voornaam"], PDO::PARAM_STR);
+      $this->db->bind(':Tussenvoegsel', $post["Tussenvoegsel"], PDO::PARAM_STR);
+      $this->db->bind(':Achternaam', $post["Achternaam"], PDO::PARAM_STR);
+      $this->db->bind(':IsVolwassen', $post["IsVolwassen"], PDO::PARAM_STR);
+      $this->db->bind(':Mobiel', $post["Mobiel"], PDO::PARAM_STR);
+      $this->db->bind(':Email', $post["Email"], PDO::PARAM_STR);
+
+      return $this->db->execute();
+    }
+
   }
 
 ?>
