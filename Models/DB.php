9779ba63c146fb687876d23c45f7271b7de6070f
PHPMADEZ <113000803+PHPMADEZ@users.noreply.github.com>
2022-12-14 11:42:16 +0100

    Models/db
    What Changed?
    
    Addition:
    
    - necessary project files

---
+++ Models/DB.php
@@ -0,0 +1,23 @@
+<?php
+
+class Database {
+
+    /*
+    -- connects to the database
+    - @return $pdo - the connection to the database
+    */
+    protected function connect() {
+        try {
+
+            $username = "root";
+            $password = "";
+            $db = new PDO('mysql:host=localhost;dbname=examen-2022-uitslag', $username, $password);
+            return $db;
+        } 
+        catch (PDOException $e) {
+            print "Error!: " . $e->getMessage() . "<br/>";
+            die();
+        }
+    }
+
+}
